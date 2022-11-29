85dc76151ee542937ae8497c1ef6181bf39f492c
Andy LaBorde <32881770+AndyLaBorde@users.noreply.github.com>
2022-11-28 20:41:32 -0700

    fixed employee tests & passing

--- _tests_/employe.test.js
+++ _tests_/employe.test.js
@@ -5,26 +5,39 @@ const employeeObj = new Employee("Andy", 111, "andy@testmail.com", "Employee");
 describe('Employee', ()=>{
     describe('getName', () => {
         it('it should return the new employee name', () => {
-            expect(employeeObj).getName().toEqual("Andy")
+
+            const result = employeeObj.getName(employeeObj);
+
+            expect(result).toEqual("Andy");
         })
     })
     
 
     describe('getEmail', () => {
         it('should return the new employee email', () => {
-            expect(employeeObj).getEmail().toEqual("andy@testmail.com")
+            const result = employeeObj.getEmail(employeeObj);
+
+            expect(result).toEqual("andy@testmail.com");
+
         })
     })
 
     describe('getId', () => {
         it('should return the new employee id', () => {
-            expect(employeeObj).getId().toEqual(111)
+
+            const result = employeeObj.getId(employeeObj);
+
+            expect(result).toEqual(111)
         })
     })
 
     describe('getRole', () => {
         it('should return the new employee role', () => {
-            expect(employeeObj).getName().toEqual("Employee")        
+
+            const result = employeeObj.getRole(employeeObj);
+
+            expect(result).toEqual("Employee");
+
         })
     })
 });
